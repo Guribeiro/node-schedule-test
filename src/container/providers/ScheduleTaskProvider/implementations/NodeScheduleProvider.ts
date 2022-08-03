b6836e1e19e09070d431df29dd0f96ac05b16801
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2022-08-03 08:21:48 -0300

    style: config linting with eslint, prettier and editorconfig

--- src/container/providers/ScheduleTaskProvider/implementations/NodeScheduleProvider.ts
+++ src/container/providers/ScheduleTaskProvider/implementations/NodeScheduleProvider.ts
@@ -1,14 +1,14 @@
-import {scheduleJob} from 'node-schedule';
-import ScheduleProvider from '../models/ScheduleProvider';
+import { scheduleJob } from "node-schedule";
+import ScheduleProvider from "../models/ScheduleProvider";
 
 class NodeScheduleProvider implements ScheduleProvider {
-	public async schedule(date: Date): Promise<void> {
-		const job = scheduleJob(date, () => {
-			console.log('Esse log foi disparado 5 segundos após a sua chamada')
-		});
-	  
-		job.nextInvocation();
-	}
+  public async schedule(date: Date): Promise<void> {
+    const job = scheduleJob(date, () => {
+      console.log("Esse log foi disparado 5 segundos após a sua chamada");
+    });
+
+    job.nextInvocation();
+  }
 }
 
 export default NodeScheduleProvider;
